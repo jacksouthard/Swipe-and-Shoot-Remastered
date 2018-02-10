935517cd87643f218788c039ec8b2897eb75c7ec
Jack Southard <jacksouthard265@gmail.com>
2018-02-10 09:00:41 -0800

    First vechicle physics

---
+++ Assets/Vechicle.cs
@@ -0,0 +1,190 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vechicle : MonoBehaviour {
+	bool grounded = false;
+
+	int targetSpeedPercent = 0;
+	int targetRotPercentage = 0;
+
+	float curWheelSpeed = 0f;
+
+	[Header("Turning")]
+	public float turnSpeed;
+	public float rotationSpeedLimiter = 1f;
+	public float rotationSpeedLimitRatio;
+
+	[Header("Speed")]
+	public float maxSpeed;
+	public float acceleration;
+	public float decceleration;
+
+	[Header("Drag")]
+	public float groundedDrag;
+	public float groundedAngDrag;
+	public float airDrag;
+	public float airAngDrag;
+
+	// wheels
+	List<Wheel> steeringWheels = new List<Wheel>();
+	List<Wheel> drivingWheels = new List<Wheel>();
+
+	Rigidbody rb;
+
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+
+		UpdateDrag ();
+
+		// init wheels
+		Wheel[] allWheels = GetComponentsInChildren<Wheel> ();
+		foreach (var wheel in allWheels) {
+			if (wheel.name.Contains ("D")) {
+				drivingWheels.Add (wheel);
+			} else if (wheel.name.Contains ("S")) {
+				steeringWheels.Add (wheel);
+			}
+		}
+	}
+
+	void Update () {
+		// forward / backwards
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			targetSpeedPercent = 1;
+		} else if (Input.GetKey (KeyCode.DownArrow)) {
+			targetSpeedPercent = -1;
+		} else {
+			targetSpeedPercent = 0;
+		}
+
+		if (targetSpeedPercent == 0f) {
+			// deccelerate if no input
+			if (curWheelSpeed > 0) {
+				curWheelSpeed -= decceleration;
+			} else if (curWheelSpeed < 0) {
+				curWheelSpeed += decceleration;
+			}
+		}
+		if (targetSpeedPercent == 1) {
+			// accelerate if forward input
+			if (curWheelSpeed >= 0) {
+				curWheelSpeed += acceleration;
+			} else if (curWheelSpeed < 0) {
+				curWheelSpeed += decceleration;
+			}
+		}
+		if (targetSpeedPercent == -1) {
+			// accelerate reverse if backward input
+			if (curWheelSpeed >= 0) {
+				curWheelSpeed -= decceleration;
+			} else if (curWheelSpeed < 0) {
+				curWheelSpeed -= acceleration;
+			}
+		}
+
+		curWheelSpeed = Mathf.Clamp (curWheelSpeed, -1f, 1f);
+
+		// steering
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			targetRotPercentage = 1;
+		} else if (Input.GetKey (KeyCode.LeftArrow)) {
+			targetRotPercentage = -1;
+		} else {
+			targetRotPercentage = 0;
+		}
+	}
+
+	void FixedUpdate () {
+		CheckGrounded ();
+		if (grounded) {
+			ApplyRotation ();
+			ApplyDrivingWheelForce ();
+			ApplySteeringWheelForce ();
+		}
+	}
+
+	void ApplyRotation () {
+		float wheelsRatio = 0f;
+		foreach (var wheel in steeringWheels) {
+			if (wheel.onGround) {
+				wheelsRatio += 1f / steeringWheels.Count;
+			}
+		}
+		rb.AddRelativeTorque (Vector3.up * targetRotPercentage * turnSpeed * Mathf.Abs (curWheelSpeed) * wheelsRatio);
+		rotationSpeedLimiter = Mathf.Clamp01(Mathf.Abs(rotationSpeedLimitRatio / rb.angularVelocity.y));
+	}
+
+	void ApplyDrivingWheelForce () {
+		float wheelsRatio = 0f;
+		foreach (var wheel in drivingWheels) {
+			if (wheel.onGround) {
+				wheelsRatio += 1f / drivingWheels.Count;
+			}
+		}
+		Vector3 force = Vector3.right * maxSpeed * curWheelSpeed * wheelsRatio * rotationSpeedLimiter;
+		rb.AddRelativeForce (force);
+	}
+
+	void ApplySteeringWheelForce () {
+		float wheelsRatio = 0f;
+		foreach (var wheel in steeringWheels) {
+			if (wheel.onGround) {
+				wheelsRatio += 1f / steeringWheels.Count;
+			}
+		}
+		Vector3 force = Vector3.right * maxSpeed * curWheelSpeed * wheelsRatio * rotationSpeedLimiter;
+		rb.AddRelativeForce (force);
+	}
+
+	void CheckGrounded () {
+		int wheelsGround = 0;
+		foreach (var wheel in drivingWheels) {
+			if (wheel.onGround) {
+				wheelsGround++;
+			}
+		}
+		foreach (var wheel in steeringWheels) {
+			if (wheel.onGround) {
+				wheelsGround++;
+			}
+		}
+
+		if (wheelsGround >= 2) {
+			grounded = true;
+		} else {
+			grounded = false;
+		}
+
+		UpdateDrag ();
+	}
+
+	void UpdateDrag () {
+		if (grounded) {
+			rb.drag = groundedDrag;
+			rb.angularDrag = groundedAngDrag;
+		} else {
+			rb.drag = airDrag;
+			rb.angularDrag = airAngDrag;
+		}
+	}
+
+	// detect wheel colisions
+	void OnCollisionStay (Collision coll) {
+		foreach (var contactPoint in coll.contacts) {
+			GameObject myCollGO = contactPoint.thisCollider.gameObject;
+			if (myCollGO.GetComponent<Wheel> () != null) {
+				myCollGO.GetComponent<Wheel> ().onGround = true;
+			}
+		}
+	}
+
+	void OnCollisionExit (Collision coll) {
+		foreach (var wheel in drivingWheels) {
+			wheel.onGround = false;
+		}
+		foreach (var wheel in steeringWheels) {
+			wheel.onGround = false;
+		}
+	}
+}
---
+++ Assets/Wheel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wheel : MonoBehaviour {
+	Vechicle vechicle;
+
+	public bool onGround = false;
+
+	void Start () {
+		vechicle = GetComponentInParent<Vechicle> ();
+	}
+	
+//	void OnCollisionEnter (Collision coll) {
+//		print ("ent");
+//		onGround = true;
+//	}
+//
+//	void OnCollisionExit (Collision coll) {
+//		onGround = false;
+//	}
+}
