ba125cc792fdd14ad344559a1bb6047a5760181f
Jack Southard <jacksouthard265@gmail.com>
2018-02-11 07:41:06 -0800

    Add muzzleflashes

--- Assets/Scripts/BulletRay.cs
+++ Assets/Scripts/BulletRay.cs
@@ -9,6 +9,7 @@ public class BulletRay : MonoBehaviour {
 	float timer;
 
 	LineRenderer lr;
+	Transform flash;
 
 	public void Init (Vector3 start, Vector3 end, float time) {
 		lifeTime = time;
@@ -17,6 +18,12 @@ public class BulletRay : MonoBehaviour {
 		Vector3[] positions = new Vector3[2];
 		positions [0] = start;
 		positions [1] = end;
+
+		flash = transform.GetChild (0);
+		flash.position = start;
+//		Vector3 dir = (end - start).normalized;
+		flash.transform.LookAt (end);
+
 		lr.SetPositions (positions);
 
 		live = true;
