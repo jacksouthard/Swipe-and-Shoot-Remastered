4d2083567efef7c6dce78fec594d16c6f4d19701
Jack Southard <jacksouthard265@gmail.com>
2018-02-10 20:32:31 -0800

    Add basic spawner class

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+	public static EnemySpawner instance;
+
+	public GameObject enemyPrefab;
+	public int maxEnemies;
+	public float spawnRate;
+
+	int enemyCount = 0;
+	float spawnTimer;
+
+	List<Vector3> spawnPoints = new List<Vector3>();
+
+	void Awake () {
+		instance = this;
+	}
+
+	void Start () {
+		for (int i = 0; i < transform.childCount; i++) {
+			Transform child = transform.GetChild (i);
+			if (child.name.Contains ("SpawnPoint")) {
+				spawnPoints.Add (child.position);
+			}
+		}
+
+		spawnTimer = spawnRate;
+	}
+	
+	void Update () {
+		if (enemyCount < maxEnemies) {
+			spawnTimer -= Time.deltaTime;
+			if (spawnTimer <= 0f) {
+				spawnTimer = spawnRate;
+
+				SpawnEnemy ();
+			}
+		}
+	}
+
+	void SpawnEnemy () {
+		int index = Random.Range (0, spawnPoints.Count);
+		Vector3 spawnPoint = spawnPoints [index];
+
+		GameObject enemy = Instantiate (enemyPrefab, spawnPoint, Quaternion.identity, transform);
+
+		enemyCount++;
+	}
+}
