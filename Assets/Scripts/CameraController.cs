8195a98bd0b12e2506185ff4dfb40318426d4372
Nathan Jew <nathankjew@gmail.com>
2018-02-10 21:06:32 -0800

    Camera lerping

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+	public float speed;
+
+	Transform player;
+	Vector3 offset;
+
+	void Awake() {
+		player = GameObject.FindObjectOfType<PlayerController> ().transform;
+		offset = transform.position - player.position;
+	}
+
+	void LateUpdate() {
+		transform.position = Vector3.Lerp (transform.position, player.position + offset, Time.deltaTime * speed);
+	}
+}
