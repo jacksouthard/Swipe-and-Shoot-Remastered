aaaf99b09b1767fb36d40f5366bb934b2c529e63
Jack Southard <jacksouthard265@gmail.com>
2018-02-10 18:38:10 -0800

    Add basic weapon script

---
+++ Assets/Scripts/BulletRay.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletRay : MonoBehaviour {
+	bool live = false;
+
+	float lifeTime;
+	float timer;
+
+	LineRenderer lr;
+
+	public void Init (Vector3 start, Vector3 end, float time) {
+		lifeTime = time;
+
+		lr = GetComponent<LineRenderer> ();
+		Vector3[] positions = new Vector3[2];
+		positions [0] = start;
+		positions [1] = end;
+		lr.SetPositions (positions);
+
+		live = true;
+	}
+	
+	void Update () {
+		if (live) {
+			lifeTime -= Time.deltaTime;
+			if (lifeTime <= 0f) {
+				Destroy (gameObject);
+			}
+		}
+	}
+}
