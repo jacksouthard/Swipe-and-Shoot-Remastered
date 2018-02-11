eaf44e66d4dd2e1d9e5471ceb9d2996cc81e76b0
Jack Southard <jacksouthard265@gmail.com>
2018-02-10 19:22:42 -0800

    Standardized weapons and added basic pickup

---
+++ Assets/Scripts/WeaponManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponManager : MonoBehaviour {
+	public static WeaponManager instance;
+
+	public WeaponData[] weaponDatas;
+
+	void Awake () {
+		instance = this;
+	}
+
+	public WeaponData GetRandomData () {
+		int index = Random.Range (0, weaponDatas.Length);
+		return weaponDatas [index];
+	}
+
+	[System.Serializable]
+	public class WeaponData {
+		public string name;
+		public GameObject prefab;
+		public Mesh mesh;
+	}
+}
---
+++ Assets/Scripts/WeaponPickup.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponPickup : MonoBehaviour {
+	public WeaponManager.WeaponData weaponData;
+
+	void Start () {
+		weaponData = WeaponManager.instance.GetRandomData ();
+		UpdateRendering ();
+	}
+
+	void UpdateRendering () {
+		GetComponent<MeshFilter> ().mesh = weaponData.mesh;
+	}
+}
