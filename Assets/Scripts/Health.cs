56a6d602147e9fc86d307f512cf3fbcfb9723ddb
Jack Southard <jacksouthard265@gmail.com>
2018-02-11 08:00:19 -0800

    Add basic death states

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,15 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Health : MonoBehaviour {
-	bool dead = false;
-
 	public float maxHealth;
 	public float regenSpeed;
 	public float regenWait;
+	public float dyingTimer;
+	public float decayTimer;
 
-	[Header("Debug")]
+	public enum State
+	{
+		Alive,
+		Dying,
+		Decaying
+	};
+	public State state = State.Alive;
 	public float waitTimer;
 	public float health;
 
@@ -18,7 +25,7 @@ public class Health : MonoBehaviour {
 	}
 	
 	void Update () {
-		if (!dead) {
+		if (state == State.Alive) {
 			if (health < maxHealth) {
 				if (waitTimer <= 0f) {
 					// start regening
@@ -30,6 +37,20 @@ public class Health : MonoBehaviour {
 					// incriment timer
 					waitTimer -= Time.deltaTime;
 				}
+			}
+		}
+
+		if (state == State.Dying) {
+			dyingTimer -= Time.deltaTime;
+			if (dyingTimer <= 0f) {
+				Decay ();
+			}
+		}
+
+		if (state == State.Decaying) {
+			decayTimer -= Time.deltaTime;
+			if (decayTimer <= 0f) {
+				Destroy (gameObject);
 			}
 		}
 	}
@@ -44,12 +65,41 @@ public class Health : MonoBehaviour {
 	}
 
 	void Die () {
-		dead = true;
+		state = State.Dying;
 		if (GetComponent<PlayerController> () != null) {
 			// handel player death
 			print ("Player Death");
-		} else {
-			Destroy (gameObject);
+			return;
+		}
+
+		// if enemy
+		if (GetComponent<EnemyController>() != null) {
+			GetComponent<NavMeshAgent> ().enabled = false;
+			GetComponent<EnemyController> ().enabled = false;
+			GetComponentInChildren<ShootingController> ().enabled = false;
+		}
+	}
+
+	void Decay () {
+		state = State.Decaying;
+
+		// disable colliders
+		if (GetComponent<Collider> () != null) {
+			GetComponent<Collider> ().enabled = false;
+		}
+
+		// children colliders
+		Collider[] childColls = GetComponentsInChildren<Collider>();
+		foreach (var coll in childColls) {
+			coll.enabled = false;
+		}
+
+		// set color to black
+		MeshRenderer[] mrs = GetComponentsInChildren<MeshRenderer>();
+		foreach (var mr in mrs) {
+			foreach (var mat in mr.materials) {
+				mat.color = Color.black;
+			}
 		}
 	}
 }
