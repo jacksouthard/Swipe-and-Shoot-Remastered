44e44ad8b4a143e089e7d33c73817ea3d5c6b892
Jack Southard <jacksouthard265@gmail.com>
2018-02-11 11:23:10 -0800

    Add bullet force

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -87,8 +87,14 @@ public class Weapon : MonoBehaviour {
 				} else if (hitGO.GetComponent<Health> () != null) {
 					hitGO.GetComponent<Health> ().TakeDamage (damage);
 				}
-			}
 
+				// apply bullet force
+				if (hitGO.GetComponent<Rigidbody> () != null) {
+					// has rigid body
+					Vector3 force = direction.normalized * damage * 3000f; // default force multiplier
+					hitGO.GetComponent<Rigidbody> ().AddForceAtPosition (force, end);
+				}
+			}
 
 			GameObject bulletRay = Instantiate (bulletRayPrefab, transform.position, Quaternion.identity, transform);
 			bulletRay.GetComponent<BulletRay> ().Init (start, end, 0.05f);
