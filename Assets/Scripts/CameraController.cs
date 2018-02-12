34384830fe1053389ab65a012263108c6cb076f8
Jack Southard <jacksouthard265@gmail.com>
2018-02-12 14:35:19 -0800

    Add camera constriants

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour {
+	[Header("Constriants")]
+	public float radius;
+	public float heightMinOffset;
+	public float heightMaxOffset;
+
+	[Header("Speed")]
 	public float speed;
 
 	Transform player;
@@ -14,6 +20,16 @@ public class CameraController : MonoBehaviour {
 	}
 
 	void LateUpdate() {
-		transform.position = Vector3.Lerp (transform.position, player.position + offset, Time.deltaTime * speed);
+		Vector2 playerPos2D = new Vector2 (player.position.x, player.position.z);
+		float curRadius = playerPos2D.magnitude;
+		if (curRadius > radius) {
+			// outside of allowed zone
+			playerPos2D = playerPos2D.normalized * radius;
+		}
+
+		float height = Mathf.Clamp (player.position.y, heightMinOffset, heightMaxOffset);
+		Vector3 targetPos = new Vector3 (playerPos2D.x, height, playerPos2D.y);
+
+		transform.position = Vector3.Lerp (transform.position, targetPos + offset, Time.deltaTime * speed);
 	}
 }
