885889f8e22a60fef7dfa664ca31f5b7325b3b08
Nathan Jew <nathankjew@gmail.com>
2018-02-15 15:43:40 -0800

    At ease position

--- Assets/Scripts/ShootingController.cs
+++ Assets/Scripts/ShootingController.cs
@@ -86,6 +86,8 @@ public class ShootingController : MonoBehaviour {
 	
 		if (target != null) {
 			RotateTowards (target);
+		} else {
+			ResetRotation ();
 		}
 	}
 
@@ -108,6 +110,10 @@ public class ShootingController : MonoBehaviour {
 		if (canRotateParent) {
 			transform.parent.rotation = Quaternion.Lerp (transform.parent.rotation, Quaternion.Euler (parentRotation.eulerAngles.x, angle, parentRotation.eulerAngles.z), Time.deltaTime * parentSpeed);
 		}
+	}
+
+	void ResetRotation() {
+		transform.localRotation = Quaternion.Lerp (transform.localRotation, Quaternion.identity, Time.deltaTime * parentSpeed);
 	}
 
 	float ClampedAngle(float angle) {
