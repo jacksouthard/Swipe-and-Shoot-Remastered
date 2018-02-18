7ecff6acd708c484d7114a2286f2e309d5e226c9
Nathan Jew <nathankjew@gmail.com>
2018-02-17 21:21:46 -0800

    Level completion UI

--- Assets/Scripts/LevelProgressManager.cs
+++ Assets/Scripts/LevelProgressManager.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class LevelProgressManager : MonoBehaviour {
 	public static LevelProgressManager instance;
 
+	public GameObject winScreen;
+
 	public bool isComplete;
 
 	void Awake() {
 		instance = this;
+		winScreen.SetActive (false);
 	}
 
 	public void CompleteLevel() {
+		winScreen.SetActive (true);
 		isComplete = true;
 		GameManager.instance.EndLevel ();
 	}
