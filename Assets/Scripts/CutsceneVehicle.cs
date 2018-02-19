8eb281ecc4722db14761adebdcca1cfafd15f418
Nathan Jew <nathankjew@gmail.com>
2018-02-18 18:05:47 -0800

    Added cutscene vehicles

---
+++ Assets/Scripts/CutsceneVehicle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutsceneVehicle : Rideable {
+	Animator anim;
+
+	void Awake() {
+		anim = GetComponent<Animator> ();
+		anim.enabled = false;
+		base.Initiate ();
+	}
+
+	public override void Mount (GameObject _mounter) {
+		base.Mount (_mounter);
+		anim.enabled = true;
+	}
+
+	//call this at the end of the animation
+	public void OnAnimationEnd() {
+		LevelProgressManager.instance.CompleteLevel ();
+	}
+}
