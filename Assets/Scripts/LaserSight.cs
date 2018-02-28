1a14db1f645efa35d4862eb9be908c6aac7e05f6
Jack Southard <jacksouthard265@gmail.com>
2018-02-28 09:21:18 -0800

    Add KAR98 sniper rifle and laser sight support

---
+++ Assets/Scripts/LaserSight.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserSight : MonoBehaviour {
+	public float maxRange = 200f;
+	LineRenderer lr;
+
+	void Start () {
+		lr = GetComponent<LineRenderer> ();
+	}
+	
+	void Update () {
+		RaycastHit hit;
+		Physics.Raycast(transform.position, transform.forward, out hit, maxRange);
+
+		if (hit.collider != null) {
+			lr.positionCount = 2;	
+			lr.SetPosition (0, transform.position);
+			lr.SetPosition (1, hit.point);
+		} else {
+			lr.positionCount = 0;
+		}
+	}
+}
