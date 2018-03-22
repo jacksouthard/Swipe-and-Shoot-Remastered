f398144be4e41e9b8d7236076fddc0b0109647ba
Nathan Jew <nathankjew@gmail.com>
2018-03-21 23:47:14 -0700

    Temp title screen stuff

--- Assets/Scripts/TitleScreen.cs
+++ Assets/Scripts/TitleScreen.cs
@@ -3,9 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TitleScreen : MonoBehaviour {
+	public AudioSource riser;
+	public AudioSource music;
+
 	public void StartGame () {
 		//for now, we assume that this is the user's first time if they haven't beaten boot camp
 		int levelToLoad = (GameProgress.farthestLevel != 0) ? 1 : 2; //1 = main menu scene, 2 = bootcamp
 		SceneFader.FadeToScene(levelToLoad, Color.black);
+	}
+
+	public void StartRiser() {
+		riser.Play ();
+	}
+
+	public void StartMusic() {
+		riser.Stop ();
+		music.Play ();
 	}
 }
