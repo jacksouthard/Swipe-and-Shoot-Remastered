bfc73d14aac6e449e8cb9219133ab8cde858d51d
Nathan Jew <nathankjew@gmail.com>
2018-04-04 21:23:26 -0700

    Looping music on Level 5

--- Assets/Scripts/MissileLauncher.cs
+++ Assets/Scripts/MissileLauncher.cs
@@ -3,10 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MissileLauncher : MonoBehaviour {
-	public void PlaySong() {
-		GetComponent<AudioSource> ().Play ();
-	}
-
 	public void FadeOut() {
 		SceneFader.FadeToColor (Color.white);
 	}
---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+	public AudioSource buildupAudio;
+	public AudioSource mainAudio;
+
+	public void PlayFromBeginning() {
+		buildupAudio.Play ();
+		mainAudio.PlayDelayed (buildupAudio.clip.length);
+	}
+
+	public void PlayMain() {
+		mainAudio.Play ();
+	}
+}
