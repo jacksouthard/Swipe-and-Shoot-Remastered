cd8e7e5b8061759454c5fd0d7831195c1bb89fc7
Nathan Jew <nathankjew@gmail.com>
2018-04-05 19:49:33 -0700

    Support of varying probabilities for enemy death sounds

--- Assets/Scripts/Data Managers/AudioManager.cs
+++ Assets/Scripts/Data Managers/AudioManager.cs
@@ -3,15 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour {
+	[System.Serializable]
+	public class DeathSound
+	{
+		public AudioClip sound;
+		public int tier;
+	}
+
 	public static AudioManager instance;
 
-	public List<AudioClip> enemyDeathSounds;
+	public List<DeathSound> enemyDeathSounds;
+	int[] tierValues = {0, 70, 99}; //hardcoded tier probabilities
 
 	void Awake() {
 		instance = this;
 	}
 
 	public AudioClip GetRandomEnemyDeathSound() {
-		return enemyDeathSounds[Random.Range(0, enemyDeathSounds.Count)];
+		int random = Random.Range (0, 100);
+		int randomTier = 0;
+
+		while((randomTier + 1) < tierValues.Length && random >= tierValues[randomTier + 1]) {
+			randomTier++;
+		}
+
+		List<DeathSound> allDataOfTier = new List<DeathSound> ();
+		foreach (DeathSound deathSound in enemyDeathSounds) {
+			if (deathSound.tier == randomTier) {
+				allDataOfTier.Add (deathSound);
+			}
+		}
+
+		if (allDataOfTier.Count == 0) {
+			// no clips in tier
+			Debug.Log ("No death sounds found in tier " + randomTier);
+			return null;
+		} else {
+			int index = Random.Range (0, allDataOfTier.Count);
+			return allDataOfTier [index].sound;
+		}
 	}
 }
