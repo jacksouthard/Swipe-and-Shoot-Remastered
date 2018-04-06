47e411c4cdc9417045f9916897a5a545b975d44c
Jack Southard <jacksouthard265@gmail.com>
2018-04-05 20:22:30 -0700

    Add audio looper and audio adjustments

---
+++ Assets/Scripts/Equipment/AudioLooper.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioLooper : MonoBehaviour {
+	int index = 0;
+	float clipTimer;
+	List<AudioClip> clips = new List<AudioClip>();
+	AudioSource source;
+
+	void Start () {
+		source = GetComponent<AudioSource> ();
+
+		Object[] objects = Resources.LoadAll ("AudioTemp");
+		foreach (var obj in objects) {
+			if (obj.GetType () == typeof(AudioClip)) {
+				clips.Add (obj as AudioClip);
+			}
+		}
+
+		PlayNext ();
+	}
+
+	void PlayNext () {
+		if (index > clips.Count - 2) {
+			index = 0;
+		}
+		index++;
+
+		source.clip = clips [index];
+		clipTimer = clips [index].length;
+		source.Play ();
+	}
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			PlayNext ();
+		} else {
+			clipTimer -= Time.deltaTime;
+			if (clipTimer <= 0f) {
+				PlayNext ();
+			}
+		}
+	}
+}
