a8fc91abd6d6638026aadcea1f04718ee9354a24
Nathan Jew <nathankjew@gmail.com>
2018-04-07 13:16:37 -0700

    Fixed stuff on the rockets

--- Assets/Scripts/Weapons/Projectile.cs
+++ Assets/Scripts/Weapons/Projectile.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour {
 	public AudioSource soundEffect;
+	GameObject model;
+	ParticleSystem trail;
 
 	public void Init(AudioClip audioClip, float pitch) {
 		soundEffect.clip = audioClip;
 		soundEffect.pitch = pitch;
 		soundEffect.Play ();
+
+		trail = GetComponent<ParticleSystem> ();
+		model = transform.GetChild (0).gameObject;
 	}
 
 	void OnCollisionEnter(Collision other) {
 		Explosion.Create (transform.position, 4, 50000, 10);
-		Destroy (gameObject);
+		Destroy (model);
+		trail.Stop ();
+		Destroy (gameObject, 2f);
 	}
 }
